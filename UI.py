aa89b59c673a66845437b643ea7894e411ed64da
EkaterinaLop <110413244+ekaterinalop@users.noreply.github.com>
2022-10-13 22:09:41 +0300

    Add files via upload

---
+++ UI.py
@@ -0,0 +1,5 @@
+P = int(input('Введите первое число  и выберите действие '))
+operations={1:'add', 2:'subtract', 3:'multiply', 4:'divide'}
+print(operations)
+Q = int(input('Введите второе число '))
+
